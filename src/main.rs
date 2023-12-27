89ad011f768b8e3874d7fc7467f26992b34d67d6
gmae199boy <kim88594544@gmail.com>
2023-12-28 00:01:37 +0900

    commit

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,88 @@ struct State {
 impl GameState for State {
   fn tick(&mut self, ctx: &mut Rltk) {
     ctx.cls();
-    ctx.print(1, 1, "Hello Rust World");
+
+    player_input(self, ctx);
+    draw_map(&self.ecs.fetch::<Vec<TileType>>(), ctx);
+
+    let positions = self.ecs.read_storage::<Position>();
+    let renderables = self.ecs.read_storage::<Renderable>();
+
+    for (pos, render) in (&positions, &renderables).join() {
+      ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+    }
+  }
+}
+
+#[derive(PartialEq, Copy, Clone)]
+enum TileType {
+  Wall,
+  Floor,
+}
+
+pub fn xy_idx(x: i32, y: i32) -> usize {
+  (y as usize * 80) + x as usize
+}
+
+fn new_map() -> Vec<TileType> {
+  let mut map = vec![TileType::Floor; 80 * 50];
+
+  // Make the boundaries walls
+  for x in 0..80 {
+    map[xy_idx(x, 0)] = TileType::Wall;
+    map[xy_idx(x, 49)] = TileType::Wall;
+  }
+  for y in 0..50 {
+    map[xy_idx(0, y)] = TileType::Wall;
+    map[xy_idx(79, y)] = TileType::Wall;
+  }
+
+  let mut rng = rltk::RandomNumberGenerator::new();
+
+  for _i in 0..400 {
+    let x = rng.roll_dice(1, 79);
+    let y = rng.roll_dice(1, 49);
+    let idx = xy_idx(x, y);
+    if idx != xy_idx(40, 25) {
+      map[idx] = TileType::Wall;
+    }
+  }
+
+  map
+}
+
+fn draw_map(map: &[TileType], ctx: &mut Rltk) {
+  let mut y = 0;
+  let mut x = 0;
+  for tile in map.iter() {
+    // Render a tile depending upon the tile type
+    match tile {
+      TileType::Floor => {
+        ctx.set(
+          x,
+          y,
+          RGB::from_f32(0.5, 0.5, 0.5),
+          RGB::from_f32(0., 0., 0.),
+          rltk::to_cp437('.'),
+        );
+      }
+      TileType::Wall => {
+        ctx.set(
+          x,
+          y,
+          RGB::from_f32(0.0, 1.0, 0.0),
+          RGB::from_f32(0., 0., 0.),
+          rltk::to_cp437('#'),
+        );
+      }
+    }
+
+    // Move the coordinates
+    x += 1;
+    if x > 79 {
+      x = 0;
+      y += 1;
+    }
   }
 }
 
@@ -27,6 +108,37 @@ struct Renderable {
   bg: RGB,
 }
 
+#[derive(Component, Debug)]
+struct Player {}
+
+fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
+  let mut positions = ecs.write_storage::<Position>();
+  let mut players = ecs.write_storage::<Player>();
+  let map = ecs.fetch::<Vec<TileType>>();
+
+  for (_player, pos) in (&mut players, &mut positions).join() {
+    let destination_idx = xy_idx(pos.x + delta_x, pos.y + delta_y);
+    if map[destination_idx] != TileType::Wall {
+      pos.x = min(79, max(0, pos.x + delta_x));
+      pos.y = min(49, max(0, pos.y + delta_y));
+    }
+  }
+}
+
+fn player_input(gs: &mut State, ctx: &mut Rltk) {
+  // Player movement
+  match ctx.key {
+    None => {} // Nothing happened
+    Some(key) => match key {
+      VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
+      VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
+      VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
+      VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
+      _ => {}
+    },
+  }
+}
+
 fn main() -> rltk::BError {
   use rltk::RltkBuilder;
   let context = RltkBuilder::simple80x50()
@@ -36,17 +148,19 @@ fn main() -> rltk::BError {
   let mut gs = State { ecs: World::new() };
   gs.ecs.register::<Position>();
   gs.ecs.register::<Renderable>();
-  for i in 0..10 {
-    gs.ecs
-      .create_entity()
-      .with(Position { x: i * 7, y: 20 })
-      .with(Renderable {
-        glyph: rltk::to_cp437('☺'),
-        fg: RGB::named(rltk::RED),
-        bg: RGB::named(rltk::BLACK),
-      })
-      .build();
-  }
+  gs.ecs.register::<Player>();
+  gs.ecs
+    .create_entity()
+    .with(Position { x: 40, y: 25 })
+    .with(Renderable {
+      glyph: rltk::to_cp437('@'),
+      fg: RGB::named(rltk::YELLOW),
+      bg: RGB::named(rltk::BLACK),
+    })
+    .with(Player {})
+    .build();
+
+  gs.ecs.insert(new_map());
 
   rltk::main_loop(context, gs)
 }
